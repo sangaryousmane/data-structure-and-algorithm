504056e824bc0b22fdcd952a68a2db1465d49bd3
ousmane <sangaryousmane@gmail.com>
2023-03-26 11:25:11 +0800

    Added case checks for letters

--- dsa-basics/basics.c
+++ dsa-basics/basics.c
@@ -4,10 +4,17 @@
 #include <ctype.h>
 #include <stdio.h>
 
+// This function uses two ways to check lower or upper case
+// it first used builtin and later used ascii
 void is_uppercase(int c){
 
-    if (isupper(c))
-        printf("%c is uppercase", c);
-    else
-        printf("%c is lowercase", c);
+//    if (isupper(c))
+//        printf("%c is uppercase", c);
+//    else
+//        printf("%c is lowercase", c);
+
+     if (c <=90)
+         printf("%c is uppercase ",c);
+     else
+         printf("%c is lowercase ", c);
 }
--- dsa-basics/main.c
+++ dsa-basics/main.c
@@ -6,6 +6,6 @@ void is_uppercase(int c);
 int main(){
 
     puts("Hello world");
-    is_uppercase('C');
+    is_uppercase(-90);
     return 0;
 }
