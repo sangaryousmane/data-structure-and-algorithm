a61e5b583f35058d2e4bbe6a85ceb68738c16b31
ousmane <sangaryousmane@gmail.com>
2023-04-02 21:43:16 +0800

    Natural numbers from and to

--- dsa-basics/main.c
+++ dsa-basics/main.c
@@ -2,10 +2,14 @@
 #include <string.h>
 
 int do_putchar(int a);
-int fib1();
+int print_to_98(int n);
 int main(void) {
 
-    fib1();
+    print_to_98(0);
+    print_to_98(98);
+    print_to_98(111);
+    print_to_98(81);
+    print_to_98(-10);
     return 0;
 }
 
