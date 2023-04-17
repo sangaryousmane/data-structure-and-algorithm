02ce08f3200a4c7dd17ac404e41185b5108bb1ce
ousmane <sangaryousmane@gmail.com>
2023-04-17 14:26:19 +0800

    Added teypedef

--- advance/task_alx.c
+++ advance/task_alx.c
@@ -21,9 +21,32 @@ void init_dog(struct dog *d, char *name, float age, char *owner) {
     }
 }
 
+void print_dog(struct dog *d){
+
+
+    if (!d){
+        exit(98);
+    }
+
+     if ((*d).name == NULL)
+       (*d).name = "(nil)";
+     if ((*d).owner == NULL){
+        (*d).owner = "(nil)";
+    } else{
+        printf("Name: %s\n", (*d).name);
+        printf("Age: %f\n", (*d).age);
+        printf("Owner: %s\n", (*d).owner);
+    }
+}
+
+
+
 void show() {
     struct dog my_dog;
 
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    my_dog.name = NULL;
+    my_dog.age = 3.5;
+    my_dog.owner = "Bob";
+    print_dog(&my_dog);
+
 }
