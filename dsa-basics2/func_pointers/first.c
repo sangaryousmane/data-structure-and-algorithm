957c528bbd0cb88d791f163e7d6c9e8f8a0c1e9e
ousmane <sangaryousmane@gmail.com>
2023-04-20 10:13:43 +0800

    Add more

--- dsa-basics2/func_pointers/first.c
+++ dsa-basics2/func_pointers/first.c
@@ -4,7 +4,24 @@
 
 #include <stdio.h>
 
-int add(int a, int b){
+int v2_add(int a, int b){
     return a + b;
 }
 
+int v2_subtract(int a, int b){
+    return (a - b);
+}
+void show_result(){
+    int result;
+//    int (*ptr)(int , int);
+//    ptr = &v2_add;
+//
+//    result = (*ptr)(4, 5);
+
+//    ptr = &v2_subtract is same as ptr = v2_subtract
+    int (*ptr)(int, int);
+    ptr = v2_subtract;
+    result = ptr(14, 5);
+
+    printf("The result is: %d", result);
+}
