a5de1f65228ffab79e0b4a61049f248f10e47526
ousmane <sangaryousmane@gmail.com>
2023-04-20 10:44:24 +0800

    Add print_name

--- dsa-basics2/func_pointers/first.c
+++ dsa-basics2/func_pointers/first.c
@@ -11,21 +11,36 @@ int v2_add(int a, int b){
 int v2_subtract(int a, int b){
     return (a - b);
 }
-void hi();
+void hi(char *name){
+    printf("Hello, %s! How are you? ", name);
+}
+void print_name(char *name, void (*f)(char *));
 void show_result(){
-    int result;
+
+    print_name("Ousmane", hi);
+//    int result;
 //    int (*ptr)(int , int);
 //    ptr = &v2_add;
 //
 //    result = (*ptr)(4, 5);
 
 //    ptr = &v2_subtract is same as ptr = v2_subtract
-    int (*ptr)(int, int);
-    ptr = v2_subtract;
-    result = ptr(14, 5);
+//    int (*ptr)(int, int);
+//    ptr = v2_subtract;
+//    result = ptr(14, 5);
+//
+//    void (*p)() = &hi;
+//
+//
+//    printf("The result is: %d", result);
+}
 
-    void (*p)() = &hi;
-
-
-    printf("The result is: %d", result);
+/**
+ * print_name: prints a name.
+ * @name: name to print
+ * @f: function to invoke to display name
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *)){
+    f(name);
 }
